db1b3a1d998ff79b6e873dd4c676e1ef278dd767
Bruno Gomes <bgomes.89@gmail.com>
2026-02-03 16:55:03 -0300

    fix ajuste css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/button";
+
 export function App() {
   return (
-    <>
-      <h1 className="text-8xl">Hello World!</h1>
-    </>
+    <main >
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button text="Click me" />
+    </main>
   )
 }
